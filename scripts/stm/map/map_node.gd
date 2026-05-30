c21799e7dcd39bb5537467a91d71f6733b3b2365
jxqxn <78200641+jxqxn@users.noreply.github.com>
2026-05-30 15:32:32 +0800

    Avoid global class lookup in map node factory

--- scripts/stm/map/map_node.gd
+++ scripts/stm/map/map_node.gd
@@ -17,7 +17,8 @@ func _init(p_floor_index: int = 0, p_node_index: int = 0, p_room_type: String = 
 
 
 static func from_dict(p_floor_index: int, p_node_index: int, data: Dictionary):
-	return StmMapNode.new(
+	var script = load("res://scripts/stm/map/map_node.gd")
+	return script.new(
 		p_floor_index,
 		p_node_index,
 		str(data.get("type", data.get("room_type", ""))),
