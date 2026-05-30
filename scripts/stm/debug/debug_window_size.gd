e30179217b20bc4791ce66d3c078fdeed313b8a0
jxqxn <78200641+jxqxn@users.noreply.github.com>
2026-05-30 10:43:38 +0800

    Use borderless windowed fullscreen for debug scene

--- scripts/stm/debug/debug_window_size.gd
+++ scripts/stm/debug/debug_window_size.gd
@@ -8,4 +8,10 @@ func _ready() -> void:
 func _apply_debug_window_mode() -> void:
 	if DisplayServer.get_name().to_lower() == "headless":
 		return
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+	var screen_index := DisplayServer.window_get_current_screen()
+	var screen_position := DisplayServer.screen_get_position(screen_index)
+	var screen_size := DisplayServer.screen_get_size(screen_index)
+	DisplayServer.window_set_position(screen_position)
+	DisplayServer.window_set_size(screen_size)
