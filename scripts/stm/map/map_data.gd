84850da779e87d8aac3172d960f7096945a8d625
jxqxn <78200641+jxqxn@users.noreply.github.com>
2026-05-30 12:07:39 +0800

    Convert fixed map data to node graph

--- scripts/stm/map/map_data.gd
+++ scripts/stm/map/map_data.gd
@@ -2,51 +2,55 @@ class_name StmMapData
 extends RefCounted
 
 # 固定 7 层测试地图。
-# floors[i]["rooms"] 是 Array[Dictionary]，每个 room 包含 type 和 next_floors。
+# floors[i]["nodes"] 是 Array[Dictionary]，每个 node 包含 type 和 next_nodes。
 # floors[i]["name"] 是楼层显示名。
-# rooms[j]["type"]: "combat" | "rest" | "boss"。
-# rooms[j]["next_floors"]: Array[int]，指向可抵达的下一层索引（空数组表示最终层）。
+# nodes[j]["type"]: "combat" | "rest" | "boss"。
+# nodes[j]["next_nodes"]: Array[Dictionary]，每项包含 floor_index 和 node_index。
 const FLOORS: Array = [
 	{
 		"name": "第 1 层",
-		"rooms": [
-			{"type": "combat", "next_floors": [1]}
+		"nodes": [
+			{"type": "combat", "next_nodes": [{"floor_index": 1, "node_index": 0}]}
 		]
 	},
 	{
 		"name": "第 2 层",
-		"rooms": [
-			{"type": "combat", "next_floors": [2]}
+		"nodes": [
+			{"type": "combat", "next_nodes": [{"floor_index": 2, "node_index": 0}]}
 		]
 	},
 	{
 		"name": "第 3 层",
-		"rooms": [
-			{"type": "combat", "next_floors": [3]}
+		"nodes": [
+			{"type": "combat", "next_nodes": [{"floor_index": 3, "node_index": 0}]}
 		]
 	},
 	{
 		"name": "第 4 层",
-		"rooms": [
-			{"type": "rest", "next_floors": [4, 5]}
+		"nodes": [
+			{"type": "rest", "next_nodes": [
+				{"floor_index": 4, "node_index": 0},
+				{"floor_index": 4, "node_index": 1},
+			]}
 		]
 	},
 	{
 		"name": "第 5 层",
-		"rooms": [
-			{"type": "combat", "next_floors": [5]}
+		"nodes": [
+			{"type": "combat", "next_nodes": [{"floor_index": 5, "node_index": 0}]},
+			{"type": "rest", "next_nodes": [{"floor_index": 5, "node_index": 0}]},
 		]
 	},
 	{
 		"name": "第 6 层",
-		"rooms": [
-			{"type": "rest", "next_floors": [6]}
+		"nodes": [
+			{"type": "rest", "next_nodes": [{"floor_index": 6, "node_index": 0}]}
 		]
 	},
 	{
 		"name": "第 7 层",
-		"rooms": [
-			{"type": "boss", "next_floors": []}
+		"nodes": [
+			{"type": "boss", "next_nodes": []}
 		]
 	},
 ]
